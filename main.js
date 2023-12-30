04d2e012a0e2bb1f96360188e460fc8e8d7524e7
Jaime Sanchez <118962371+Jaime-Sanz@users.noreply.github.com>
2023-12-30 00:49:47 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const myLibrary = [];
+
+function champion(name, kills, wins, losses) {
+    this.name = name;
+    this.kills = kills;
+    this.wins = wins;
+    this.losses = losses;
+}
+
+function addChampionToLibrary() {
+  let championName = document.getElementById("champion").value;
+  let championKills = document.getElementById("kills").value;
+  let championWins = document.getElementById("wins").value;
+  let championLosses = document.getElementById("losses").value;
+  let newChampion = new champion(championName, championKills, championWins, championLosses);
+  myLibrary.push(newChampion);
+
+  championDataToScreen();
+}
+
+function championDataToScreen() {
+    
+}
+
+//function that toggles champion add pop-up visibility
+
+const newChampion = () => {
+    onScreen = document.getElementById("addui");
+
+    if (onScreen.style.visibility === "visible") {
+        onScreen.style.visibility = "hidden";
+    } else {
+        onScreen.style.visibility = "visible";
+    }
+}
+
+//event listener that listens for submit from form to then add local data to screen
+document.getElementById("new-champion").addEventListener("submit", function(){
+    event.preventDefault();
+    addChampionToLibrary(); });;
